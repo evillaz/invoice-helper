760695621a2c201f793cf7b1765de17732108cda
evillaz <evillaz123@gmail.com>
2025-03-24 11:55:24 -0500

    Update POAGenerator.jsx

--- src/components/POAGenerator.jsx
+++ src/components/POAGenerator.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const POAGenerator = () => {
   const navigate = useNavigate();
@@ -8,11 +8,11 @@ const POAGenerator = () => {
   const invoices = useSelector((state) => state.invoices.invoices);
   const selectedInvoices = useSelector((state) => state.invoices.selectedInvoices);
 
-
   return (
     <div>
       <h1>gen</h1>
       <button
+        type="button"
         className="mt-4 px-4 py-2 bg-gray-500 text-white rounded"
         onClick={() => navigate('/', { state: { selectedInvoices, invoices } })}
       >
