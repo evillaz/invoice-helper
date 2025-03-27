f80c666a7c12a69929a3c6ad133719254bdd60a1
evillaz <evillaz123@gmail.com>
2025-03-27 16:29:10 -0500

    integrate database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import XmlConverter from './components/XmlConverter';
 import POAGenerator from './components/POAGenerator';
 import './styles/style.css';
+import { fetchInvoices } from './redux/databaseInvoiceSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchInvoices());
+  }, [dispatch]);
+  const invoices = useSelector((state) => state.databaseInvoices.invoices);
+  console.log(invoices);
+
   return (
     <>
       <Routes>
